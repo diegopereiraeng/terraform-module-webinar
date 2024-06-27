6dc678a0a45ae00706f8e5b0f22c79b444b06fc4
Diego Pereira <22509951+diegopereiraeng@users.noreply.github.com>
2024-06-26 23:43:51 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,18 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region     = "us-east-1"
+  access_key = var.access_key
+  secret_key = var.secret_key
+}
+
 module "vpc" {
   source = "./modules/vpc"
   cidr_block           = var.vpc_cidr
