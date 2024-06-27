29f8de53ccbd10348152489313df369bd873e52d
Diego Pereira <22509951+diegopereiraeng@users.noreply.github.com>
2024-06-26 23:38:42 -0300

    Create variables.tf

---
+++ modules/autoscaling_group/variables.tf
@@ -0,0 +1,27 @@
+variable "desired_capacity" {
+  description = "Desired capacity of the autoscaling group"
+  type        = number
+  default     = 1
+}
+
+variable "max_size" {
+  description = "Maximum size of the autoscaling group"
+  type        = number
+  default     = 1
+}
+
+variable "min_size" {
+  description = "Minimum size of the autoscaling group"
+  type        = number
+  default     = 1
+}
+
+variable "subnet_ids" {
+  description = "List of subnet IDs"
+  type        = list(string)
+}
+
+variable "launch_template_id" {
+  description = "Launch Template ID"
+  type        = string
+}
