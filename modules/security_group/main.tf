f8bb843fa53871c05b1caf44ccf9d331088fa11f
Diego Pereira <22509951+diegopereiraeng@users.noreply.github.com>
2024-06-26 22:55:44 -0300

    Create main.tf

---
+++ modules/security_group/main.tf
@@ -0,0 +1,27 @@
+resource "aws_security_group" "ecs_security_group" {
+  name   = var.name
+  vpc_id = var.vpc_id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = var.tags
+}
