b54a268b81d785817598379c12315851945902c7
Diego Pereira <22509951+diegopereiraeng@users.noreply.github.com>
2024-06-26 23:33:16 -0300

    Create variables.tf

---
+++ modules/ecs_task_definition/variables.tf
@@ -0,0 +1,54 @@
+variable "family" {
+  description = "Task definition family"
+  type        = string
+  default     = "time-for-heroes"
+}
+
+variable "cpu" {
+  description = "CPU for the task definition"
+  type        = number
+  default     = 256
+}
+
+variable "memory" {
+  description = "Memory for the task definition"
+  type        = number
+  default     = 512
+}
+
+variable "execution_role_arn" {
+  description = "ARN of the execution role"
+  type        = string
+}
+
+variable "task_role_arn" {
+  description = "ARN of the task role"
+  type        = string
+}
+
+variable "container_name" {
+  description = "Container name"
+  type        = string
+}
+
+variable "container_image" {
+  description = "Container image"
+  type        = string
+}
+
+variable "container_port" {
+  description = "Container port"
+  type        = number
+}
+
+variable "container_cpu" {
+  description = "CPU for the container"
+  type        = number
+  default     = 128
+}
+
+variable "container_memory" {
+  description = "Memory for the container"
+  type        = number
+  default     = 256
+}
