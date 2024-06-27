9db0ed12faff36e3a8c50878d21736b660f30c95
Diego Pereira <22509951+diegopereiraeng@users.noreply.github.com>
2024-06-26 23:35:52 -0300

    Create outputs.tf

---
+++ modules/iam_roles/outputs.tf
@@ -0,0 +1,14 @@
+output "ecs_task_role_arn" {
+  description = "ECS Task Role ARN"
+  value       = aws_iam_role.ecs_task_role.arn
+}
+
+output "ecs_task_execution_role_arn" {
+  description = "ECS Task Execution Role ARN"
+  value       = aws_iam_role.ecs_task_execution_role.arn
+}
+
+output "instance_profile_name" {
+  description = "Instance Profile Name"
+  value       = aws_iam_instance_profile.ecs_instance_profile.name
+}
