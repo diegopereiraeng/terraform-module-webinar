328bf7146d1e0646ca85a95d535decf701d6dbb3
Diego Pereira <22509951+diegopereiraeng@users.noreply.github.com>
2024-06-26 23:42:49 -0300

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,3 @@
-
 output "vpc_id" {
   description = "VPC ID"
   value       = module.vpc.vpc_id
@@ -22,4 +21,29 @@ output "ecs_cluster_id" {
 output "ecs_service_name" {
   description = "ECS Service Name"
   value       = module.ecs.ecs_service_name
+}
+
+output "launch_template_id" {
+  description = "Launch Template ID"
+  value       = module.launch_template.launch_template_id
+}
+
+output "autoscaling_group_id" {
+  description = "Autoscaling Group ID"
+  value       = module.autoscaling_group.autoscaling_group_id
+}
+
+output "load_balancer_arn" {
+  description = "Load Balancer ARN"
+  value       = module.load_balancer.load_balancer_arn
+}
+
+output "target_group_arn" {
+  description = "Target Group ARN"
+  value       = module.target_group.target_group_arn
+}
+
+output "listener_arn" {
+  description = "Listener ARN"
+  value       = module.listener.listener_arn
 }
