93d19f414a499984013a0598f09702726e79e1c8
Diego Pereira <22509951+diegopereiraeng@users.noreply.github.com>
2024-06-26 23:39:11 -0300

    Create main.tf

---
+++ modules/load_balancer/main.tf
@@ -0,0 +1,16 @@
+resource "aws_lb" "main" {
+  name               = "main-nlb"
+  internal           = false
+  load_balancer_type = "network"
+  subnets            = var.subnet_ids
+
+  security_groups = [var.security_group_id]
+
+  enable_deletion_protection = false
+
+  lifecycle {
+    create_before_destroy = true
+  }
+
+  tags = var.tags
+}
