962837f79777bec67de2a17a557bd75b243cd200
Diego Pereira <22509951+diegopereiraeng@users.noreply.github.com>
2024-06-27 00:10:20 -0300

    Create variables.tf

---
+++ modules/key_pair/variables.tf
@@ -0,0 +1,9 @@
+variable "key_name" {
+  description = "Name of the SSH key pair"
+  type        = string
+}
+
+variable "public_key_path" {
+  description = "Path to the SSH public key file"
+  type        = string
+}
