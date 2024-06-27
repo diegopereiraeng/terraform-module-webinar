4fe6495cc4d4a11ee623c630f610d0af3a60b2e6
Diego Pereira <22509951+diegopereiraeng@users.noreply.github.com>
2024-06-26 22:58:14 -0300

    Create variables.tf

---
+++ modules/ecs/variables.tf
@@ -0,0 +1,40 @@
+variable "cluster_name" {
+  description = "O nome do cluster ECS"
+  type        = string
+}
+
+variable "service_name" {
+  description = "O nome do serviço ECS"
+  type        = string
+}
+
+variable "task_definition" {
+  description = "O ARN da definição de tarefa a ser usada para o serviço"
+  type        = string
+}
+
+variable "desired_count" {
+  description = "O número de instâncias da definição de tarefa a serem executadas"
+  type        = number
+  default     = 1
+}
+
+variable "target_group_arn" {
+  description = "O ARN do grupo de destino do balanceador de carga"
+  type        = string
+}
+
+variable "container_name" {
+  description = "O nome do contêiner a ser associado ao balanceador de carga"
+  type        = string
+}
+
+variable "container_port" {
+  description = "A porta no contêiner a ser associada ao balanceador de carga"
+  type        = number
+}
+
+variable "tags" {
+  description = "Tags a serem aplicadas aos recursos"
+  type        = map(string)
+}
