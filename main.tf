30f7174c059f1a9a84a8fce6d6a272ff869b1023
Diego Pereira <22509951+diegopereiraeng@users.noreply.github.com>
2024-06-26 23:07:01 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,26 +1,26 @@
 module "vpc" {
   source = "./modules/vpc"
-
   cidr_block           = var.vpc_cidr
   enable_dns_hostnames = true
   tags                 = var.tags
 }
 
-module "subnet1" {
-  source               = "./modules/subnet"
-  vpc_id               = module.vpc.vpc_id
-  cidr_block           = "10.0.1.0/24"
-  map_public_ip_on_launch = true
-  availability_zone    = "us-east-1a"
-  tags                 = var.tags
-}
+module "subnet" {
+  source = "./modules/subnet"
 
-module "subnet2" {
-  source               = "./modules/subnet"
+  for_each = {
+    subnet1 = {
+      cidr_block        = "10.0.1.0/24"
+      availability_zone = "us-east-1a"
+    }
+    subnet2 = {
+      cidr_block        = "10.0.2.0/24"
+      availability_zone = "us-east-1b"
+    }
+  }
+
   vpc_id               = module.vpc.vpc_id
-  cidr_block           = "10.0.2.0/24"
   map_public_ip_on_launch = true
-  availability_zone    = "us-east-1b"
   tags                 = var.tags
 }
 
@@ -33,12 +33,11 @@ module "security_group" {
 
 module "ecs" {
   source          = "./modules/ecs"
-
   cluster_name    = "ecs-cluster"
   service_name    = "web-service"
-  task_definition = "arn:aws:ecs:us-east-1:123456789012:task-definition/web:1"
+  task_definition = var.task_definition
   desired_count   = 2
-  target_group_arn = "arn:aws:elasticloadbalancing:us-east-1:123456789012:targetgroup/my-targets/6d0ecf831eec9f09"
+  target_group_arn = var.target_group_arn
   container_name  = "web"
   container_port  = 80
   tags            = var.tags
