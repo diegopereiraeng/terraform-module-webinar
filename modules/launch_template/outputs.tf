e398788449e369d21c1f7b433cf00348a87fa563
Diego Pereira <22509951+diegopereiraeng@users.noreply.github.com>
2024-06-27 04:48:34 -0300

    harness

--- modules/launch_template/outputs.tf
+++ modules/launch_template/outputs.tf
@@ -3,7 +3,7 @@ output "launch_template_id" {
   value       = aws_launch_template.ecs_lt.id
 }
 
-output "public_dns" {
-  description = "Public DNS of the instances launched by this template"
-  value       = aws_instance.your_instance_name.public_dns
-}
+# output "public_dns" {
+#   description = "Public DNS of the instances launched by this template"
+#   value       = aws_instance.your_instance_name.public_dns
+# }
