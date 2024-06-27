32039517ee1209a5462bc28209ddd04dbd2762ab
Diego Pereira <22509951+diegopereiraeng@users.noreply.github.com>
2024-06-26 22:56:25 -0300

    Update variables.tf

--- modules/security_group/variables.tf
+++ modules/security_group/variables.tf
@@ -2,3 +2,13 @@ variable "name" {
   description = "O nome do grupo de segurança"
   type        = string
 }
+
+variable "vpc_id" {
+  description = "O ID da VPC"
+  type        = string
+}
+
+variable "tags" {
+  description = "Tags a serem aplicadas aos recursos"
+  type        = map(string)
+}
