b988a76ef1c6dc1400c27243bf189728520f2e5c
Diego Pereira <22509951+diegopereiraeng@users.noreply.github.com>
2024-06-26 23:38:27 -0300

    Create main.tf

---
+++ modules/autoscaling_group/main.tf
@@ -0,0 +1,16 @@
+resource "aws_autoscaling_group" "ecs_asg" {
+  desired_capacity     = var.desired_capacity
+  max_size             = var.max_size
+  min_size             = var.min_size
+  vpc_zone_identifier  = var.subnet_ids
+  launch_template {
+    id      = var.launch_template_id
+    version = "$Latest"
+  }
+
+  tag {
+    key                 = "Name"
+    value               = "ecs-instance"
+    propagate_at_launch = true
+  }
+}
