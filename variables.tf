74255d0ea993697e2e2a34bd5c2493518f7d3fea
Diego Pereira <22509951+diegopereiraeng@users.noreply.github.com>
2024-06-26 23:15:19 -0300

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -20,6 +20,35 @@ variable "target_group_arn" {
   type        = string
 }
 
+variable "key_name" {
+  description = "Name of the SSH key pair"
+  type        = string
+}
+
+variable "security_group_ingress_rules" {
+  description = "List of ingress rules for the security group"
+  type = list(object({
+    from_port   = number
+    to_port     = number
+    protocol    = string
+    cidr_blocks = list(string)
+  }))
+  default = [
+    {
+      from_port   = 80
+      to_port     = 80
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    },
+    {
+      from_port   = 22
+      to_port     = 22
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+  ]
+}
+
 variable "tags" {
   description = "Tags to apply to resources"
   type        = map(string)
