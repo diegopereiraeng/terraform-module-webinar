9e8b11b71833d3ac2c2ba2efe4f88d1533f65219
Diego Pereira <22509951+diegopereiraeng@users.noreply.github.com>
2024-06-26 22:57:52 -0300

    Create main.tf

---
+++ modules/ecs/main.tf
@@ -0,0 +1,22 @@
+resource "aws_ecs_cluster" "cluster" {
+  name = var.cluster_name
+  tags = var.tags
+}
+
+resource "aws_ecs_service" "service" {
+  name            = var.service_name
+  cluster         = aws_ecs_cluster.cluster.id
+  task_definition = var.task_definition
+  desired_count   = var.desired_count
+  launch_type     = "EC2"
+  tags            = var.tags
+
+  deployment_minimum_healthy_percent = 50
+  deployment_maximum_percent         = 200
+
+  load_balancer {
+    target_group_arn = var.target_group_arn
+    container_name   = var.container_name
+    container_port   = var.container_port
+  }
+}
