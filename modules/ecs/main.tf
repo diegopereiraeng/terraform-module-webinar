350f19e4671e9f160e03a0339177120c83b48c09
Diego Pereira <22509951+diegopereiraeng@users.noreply.github.com>
2025-02-20 11:54:39 -0300

    fix

--- modules/ecs/main.tf
+++ modules/ecs/main.tf
@@ -8,7 +8,7 @@ resource "aws_ecs_service" "service" {
   cluster         = aws_ecs_cluster.cluster.id
   task_definition = var.task_definition
   desired_count   = var.desired_count
-  launch_type     = "EC2"
+  launch_type     = "FARGATE"
   tags            = var.tags
 
   deployment_minimum_healthy_percent = 50
