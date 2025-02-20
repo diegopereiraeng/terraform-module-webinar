56d7a94c971d083001179b9d931bc2f4e0dffc6a
Diego Pereira <22509951+diegopereiraeng@users.noreply.github.com>
2025-02-19 23:15:23 -0300

    Update main.tf

--- modules/ecs/main.tf
+++ modules/ecs/main.tf
@@ -20,3 +20,30 @@ resource "aws_ecs_service" "service" {
     container_port   = var.container_port
   }
 }
+
+resource "harness_platform_file_store_file" "ecs_service_manifest" {
+  org_id            = var.organization_id
+  project_id        = var.project_id
+  identifier        = var.service_name
+  name              = var.service_name
+  description       = "ECS Service Definition YAML for ${var.service_name}"
+  tags              = ["provisioned:by-automation"]
+  parent_identifier = "Root"
+
+  file_content = templatefile("${path.module}/templates/banking-ecs-service-v2.yaml", {
+    ecs_service_arn                    = service.service.arn
+    service_name                       = var.service_name
+    cluster_arn                        = cluster.cluster.arn
+    target_group_arn                   = var.target_group_arn
+    container_name                     = var.container_name
+    container_port                     = var.container_port
+    desired_count                      = var.desired_count
+    task_definition                    = var.task_definition
+    deployment_maximum_percent         = 200
+    deployment_minimum_healthy_percent = 50
+  })
+
+  mime_type  = "text/yaml"
+  file_usage = "ManifestFile"
+}
+
