5aa2a271aaaca73daab5e61733cfbabee01209c9
Diego Pereira <22509951+diegopereiraeng@users.noreply.github.com>
2025-02-20 15:03:34 -0300

    fix

--- modules/ecs/main.tf
+++ modules/ecs/main.tf
@@ -3,34 +3,14 @@ resource "aws_ecs_cluster" "cluster" {
   tags = var.tags
 }
 
-resource "aws_ecs_service" "service" {
-  name            = var.service_name
-  cluster         = aws_ecs_cluster.cluster.id
-  task_definition = var.task_definition
-  desired_count   = var.desired_count
-  launch_type     = "EC2"
-  tags            = var.tags
-
-  deployment_minimum_healthy_percent = 50
-  deployment_maximum_percent         = 200
-
-  load_balancer {
-    target_group_arn = var.target_group_arn
-    container_name   = var.container_name
-    container_port   = var.container_port
-  }
-}
-
 locals {
   generated_manifest = templatefile("${path.module}/templates/banking-ecs-service-v2.yaml", {
-    ecs_service_arn                    = aws_ecs_service.service.id
     service_name                       = var.service_name
     cluster_arn                        = aws_ecs_cluster.cluster.arn
     target_group_arn                   = var.target_group_arn
     container_name                     = var.container_name
     container_port                     = var.container_port
     desired_count                      = var.desired_count
-    task_definition                    = var.task_definition
     deployment_maximum_percent         = 200
     deployment_minimum_healthy_percent = 50
   })
@@ -46,11 +26,11 @@ resource "local_file" "generated_manifest_file" {
 resource "harness_platform_file_store_file" "ecs_service_manifest" {
   org_id            = var.harness_organization_id
   project_id        = var.harness_project_id
-  identifier        = var.service_name
+  identifier        = lower(format("%s", replace(var.service_name, "-", "_")))
   name              = var.service_name
   description       = "ECS Service Definition YAML for ${var.service_name}"
   tags              = ["provisioned:by-automation"]
-  parent_identifier = "Root"
+  parent_identifier = "/diego-statestreet-test.yaml"
   
   file_content_path = local_file.generated_manifest_file.filename
   mime_type         = "text/yaml"
