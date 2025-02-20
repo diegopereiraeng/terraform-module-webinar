40915bccdae18546b3fda3e24dc7b609e8c7568d
Diego Pereira <22509951+diegopereiraeng@users.noreply.github.com>
2025-02-20 01:30:14 -0300

    outputs

--- modules/ecs/outputs.tf
+++ modules/ecs/outputs.tf
@@ -7,3 +7,28 @@ output "ecs_service_name" {
   description = "O nome do serviço ECS"
   value       = aws_ecs_service.service.name
 }
+
+output "ecs_service_arn" {
+  description = "The ARN of the ECS service"
+  value       = aws_ecs_service.service.id
+}
+
+output "ecs_container_name" {
+  description = "The container name as specified in the ECS service load_balancer block"
+  value       = var.container_name
+}
+
+output "ecs_container_port" {
+  description = "The container port as specified in the ECS service load_balancer block"
+  value       = var.container_port
+}
+
+output "ecs_desired_count" {
+  description = "The desired number of task instances for the ECS service"
+  value       = var.desired_count
+}
+
+output "ecs_tags" {
+  description = "The tags applied to the ECS service"
+  value       = var.tags
+}
--- modules/ecs_task_definition/outputs.tf
+++ modules/ecs_task_definition/outputs.tf
@@ -1,4 +1,5 @@
 output "task_definition_arn" {
   description = "ECS Task Definition ARN"
-  value       = aws_ecs_task_definition.task_definition.arn
+  value       = aws_ecs_task_definition.task_definition.id
 }
+
