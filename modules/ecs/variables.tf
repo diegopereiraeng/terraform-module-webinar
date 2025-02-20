8951d072da247e447517d00225ea4d65a057bc73
Diego Pereira <22509951+diegopereiraeng@users.noreply.github.com>
2025-02-19 23:16:40 -0300

    Update variables.tf

--- modules/ecs/variables.tf
+++ modules/ecs/variables.tf
@@ -1,3 +1,15 @@
+
+
+variable "harness_organization_id" {
+  description = "Harness org identifier"
+  type        = string
+}
+
+variable "harness_project_id" {
+  description = "Harness project identifier"
+  type        = string
+}
+
 variable "cluster_name" {
   description = "O nome do cluster ECS"
   type        = string
