b0eef430a1fa1547045f8503678c1a0d61532448
Diego Pereira <22509951+diegopereiraeng@users.noreply.github.com>
2025-02-20 01:05:19 -0300

    Revert "Update main.tf"
    This reverts commit 25685b49b89c0aa285507ff2e052a067458bfa26.

--- main.tf
+++ main.tf
@@ -4,17 +4,8 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 5.0"
     }
-    harness = {
-      source = "harness/harness"
-      version = "0.35.5"
-    }
   }
   backend "s3" {}
-}
-
-provider "harness" {
-  account_id       = var.account_id
-  platform_api_key = var.platform_api_key
 }
 
 provider "aws" {
